5a4c696973a0a3ae0e7078f37af51feea4e12abe
Patrick Cheng <pat.rick.cheng@hotmail.com>
2018-08-07 04:38:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(obj,key,value){
-  var newObj = Object.assign({},obj,{})
+  return Object.assign({},obj,{[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(){
+  
 }
